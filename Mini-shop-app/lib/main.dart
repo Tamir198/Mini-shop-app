af50f6f17b8c2a03c44d11acf54398498728a4bb
Tamir <1tamir198@gmail.com>
2020-08-25 21:04:09 +0300

    Adding provider for the cart items

--- Mini-shop-app/lib/main.dart
+++ Mini-shop-app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'providers/products_provider.dart';
 import 'package:shop_app/screens/product_detail_screen.dart';
 import 'package:shop_app/screens/product_overview_screen.dart';
 
@@ -8,17 +10,25 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato'
+    //ChangeNotifierProvider - this is my provider came from provider package
+    //EVERY child widget that will listen to the provider will rebuild on data changed, so the provider
+    //provides data to its children's to listen to
+    //This is why he provider must be above  those children's who need the data in the widget tree
+    return ChangeNotifierProvider(
+      //Products is the instance class that provided by provider
+      create: (BuildContext context) => Products(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato'
+        ),
+        home: ProductOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routName : (context) => ProductDetailScreen(),
+        },
       ),
-      home: ProductOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routName : (context) => ProductDetailScreen(),
-      },
     );
   }
 }
---
+++ Mini-shop-app/lib/widgets/product_grid.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products_provider.dart';
+import 'package:shop_app/widgets/product_item.dart';
+
+class ProductsGridView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    //Set connection to provided class and listen to changes
+    //Only this widget and sub children will get re build when the provided class data will change
+
+    //Provider.of is generic method, <Products> is the type of data I want to listen to
+    final productsData  = Provider.of<Products>(context);
+    final products = productsData.items;
+    return GridView.builder(
+      //structure of the greed (how many columns rows...)
+      //SliverGridDelegateWithFixedCrossAxisCount - show certain amount of item on the screen
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        //how many columns
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        //Space between columns
+        crossAxisSpacing: 10,
+        //Space between rows
+        mainAxisSpacing: 10,
+      ),
+      //What will be displayed on the screen
+      itemBuilder: (context, item) => ProductItem(
+
+          products[item].id,
+          products[item].title,
+          products[item].imageUrl
+
+      ),
+      itemCount: products.length,
+      //const prevent padding from being rebuild for every time build() is called
+      padding: const EdgeInsets.all(10.0),
+    );
+  }
+}
