961a944fe129fb00e299235949304c192fe253a2
Tamir <1tamir198@gmail.com>
2020-10-15 13:25:47 +0300

    Adding minor post method

--- Mini-shop-app/lib/providers/products_provider.dart
+++ Mini-shop-app/lib/providers/products_provider.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import './product.dart';
 
@@ -10,14 +13,14 @@ import './product.dart';
 //The data of the provider
 
 class Products with ChangeNotifier {
-  List <Product> _items = [
+  List<Product> _items = [
     Product(
       id: 'p1',
       title: 'Red Shirt',
       description: 'A red shirt - it is pretty red!',
       price: 29.99,
       imageUrl:
-      'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
     ),
     Product(
       id: 'p2',
@@ -25,7 +28,7 @@ class Products with ChangeNotifier {
       description: 'A nice pair of trousers.',
       price: 59.99,
       imageUrl:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
     ),
     Product(
       id: 'p3',
@@ -33,7 +36,7 @@ class Products with ChangeNotifier {
       description: 'Warm and cozy - exactly what you need for the winter.',
       price: 19.99,
       imageUrl:
-      'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
     ),
     Product(
       id: 'p4',
@@ -41,7 +44,7 @@ class Products with ChangeNotifier {
       description: 'Prepare any meal you want.',
       price: 49.99,
       imageUrl:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
     ),
   ];
 
@@ -58,39 +61,47 @@ class Products with ChangeNotifier {
     return _items.where((product) => product.isFavorite).toList();
   }
 
-
   Product findProductById(String id) {
     return _items.firstWhere((element) => element.id == id);
   }
 
   void addProduct(Product product) {
-    //Tell all ChangeNotifier listeners that data was updated
-    Product newProduct = Product(title: product.title,
-        price: product.price,
-        imageUrl: product.imageUrl,
-        description: product.description,
-        id: DateTime.now().toString());
+    const String url = 'https://udemy-shop-app-course.firebaseio.com/products.json';
+    //Converting data to json json.encode() knows how to convert maps to json
+    http.post(url,
+            body: json.encode({
+              'title': product.title,
+              'description': product.description,
+              'price': product.price,
+              'imageUrl': product.imageUrl,
+              'isFavorite': product.isFavorite,
+            }))
+        .then((response) {
+      //Tell all ChangeNotifier listeners that data was updated
+      Product newProduct = Product(
+          title: product.title,
+          price: product.price,
+          imageUrl: product.imageUrl,
+          description: product.description,
+          id: json.decode(response.body)['name']);
 
-    _items.add(newProduct);
+      _items.add(newProduct);
 
-    notifyListeners();
+      notifyListeners();
+    });
   }
 
-  void updateProduct(String id, Product newProduct){
-   int productIndex =  _items.indexWhere((product) => product.id == id);
+  void updateProduct(String id, Product newProduct) {
+    int productIndex = _items.indexWhere((product) => product.id == id);
 
-   if(productIndex >= 0){
-     _items[productIndex] = newProduct;
-     notifyListeners();
-   }
-
+    if (productIndex >= 0) {
+      _items[productIndex] = newProduct;
+      notifyListeners();
+    }
   }
 
-  void deleteProduct(String id){
+  void deleteProduct(String id) {
     _items.removeWhere((product) => product.id == id);
     notifyListeners();
   }
-
 }
-
-
